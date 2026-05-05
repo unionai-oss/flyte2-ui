40a5134bb2a1139e9ed7736e4149be55c236621a
Carina Ursu <carina@union.ai>
2026-05-05 11:52:54 -0500

    fix: report fullscreen bg color
    Signed-off-by: Carina Ursu <carina@union.ai>

--- src/components/pages/RunDetails/Reports/ReportDialog.tsx
+++ src/components/pages/RunDetails/Reports/ReportDialog.tsx
@@ -17,9 +17,9 @@ export function ReportDialog({
 }: PropsWithChildren<ReportDialogProps>) {
   return (
     <Dialog open={isOpen} onClose={closeDialog} className="relative z-10">
-      <DialogBackdrop className="fixed inset-0 bg-(--system-black)" />
+      <DialogBackdrop className="fixed inset-0 bg-black/60" />
       <div className="fixed inset-0 flex h-screen w-screen items-center justify-center p-3">
-        <DialogPanel className="h-full w-full rounded-2xl bg-(--system-white)">
+        <DialogPanel className="h-full w-full rounded-2xl bg-(--system-black)">
           {children}
         </DialogPanel>
       </div>
