55235fd0b0a0021ea354f57a8e33549351cda193
Carina Ursu <carina@union.ai>
2026-06-18 10:03:51 -0500

    Filter out links with empty urls
    Signed-off-by: Carina Ursu <carina@union.ai>

--- src/components/pages/RunDetails/ActionDashboardLinks.tsx
+++ src/components/pages/RunDetails/ActionDashboardLinks.tsx
@@ -2,22 +2,23 @@
  * © Copyright Union Systems Inc 2026. All rights reserved.
  */
 
-import { useExternalLogUrls } from '@/components/pages/RunDetails/hooks/useExternalLogUrls'
 import { ExternalLinkUrl } from '@/components/ExternalLinkUrl'
+import { useExternalLogUrls } from '@/components/pages/RunDetails/hooks/useExternalLogUrls'
+import { TaskLog_LinkType } from '@/gen/flyteidl2/core/execution_pb'
 import { ActionAttempt } from '@/gen/flyteidl2/workflow/run_definition_pb'
 import React from 'react'
-import { TaskLog_LinkType } from '@/gen/flyteidl2/core/execution_pb'
 
 export const ActionDashboardLinks: React.FC<{
   attempt?: ActionAttempt | null
 }> = ({ attempt }) => {
   const urls = useExternalLogUrls(attempt?.logInfo, TaskLog_LinkType.DASHBOARD)
+  const validUrls = urls.filter(({ url }) => Boolean(url))
 
-  return urls.length ? (
+  return validUrls.length > 0 ? (
     <div>
       <h3 className="py-2 text-sm/5 font-bold">Links</h3>
       <div className="mt-2 flex flex-row flex-wrap gap-2">
-        {urls.map((url, index) => (
+        {validUrls.map((url, index) => (
           <ExternalLinkUrl {...url} key={index} />
         ))}
       </div>
